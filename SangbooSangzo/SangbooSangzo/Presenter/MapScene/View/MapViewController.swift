60bf99777ce2e3c9b11bd32c392b205e4ba4d9be
Deokhun KIM <thekoon0456@gmail.com>
2024-04-15 16:05:23 +0900

    [Feat] 지도 뷰 추가

--- SangbooSangzo/SangbooSangzo/Presenter/MapScene/View/MapViewController.swift
+++ SangbooSangzo/SangbooSangzo/Presenter/MapScene/View/MapViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Deokhun KIM on 4/15/24.
 //
 
+import MapKit
 import UIKit
 
 import RxCocoa
@@ -13,10 +14,28 @@ import RxSwift
 final class MapViewController: RxBaseViewController {
     
     private let viewModel: MapViewModel
+    private let mapView = MKMapView()
     
     init(viewModel: MapViewModel) {
         self.viewModel = viewModel
         super.init()
     }
     
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        view.addSubviews(mapView)
+    }
+    
+    override func configureLayout() {
+        super.configureLayout()
+        mapView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        super.configureView()
+        navigationItem.title = "지도"
+    }
 }
